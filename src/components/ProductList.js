2a54d43659a8bf876519166442c23ed40954981f
happyhearts99 <happyhearts99@gmail.com>
2020-03-13 17:36:35 -0400

    Delete ProductList.js

--- src/components/ProductList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProductsList = ({ name, children }) => (
-  <div style={{display:"inline-block",width:"50%",float:"left"}}>
-    <div>{children}</div>
-  </div>
-)
-
-export default ProductsList
