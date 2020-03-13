a1f6dd94a3aed3e045e29fca79226f3a16fccb9d
happyhearts99 <happyhearts99@gmail.com>
2020-03-13 17:23:01 -0400

    Create App.js

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import ProductsContainer from './ProductsContainer'
+import CartContainer from './CartContainer'
+
+const App = () => (
+  <div>
+    <ProductsContainer />
+    <CartContainer />
+  </div>
+)
+
+export default App
