04f375bc071553c4c67a6d8d27052101a253f424
happyhearts99 <happyhearts99@gmail.com>
2020-03-13 17:32:45 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+import { getProducts } from './actions'
+import App from './containers/App'
+
+const middleware = [ thunk ];
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+store.dispatch(getProducts())
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
